f3d6b9fb063a63386431f34adccf0e24b5ec1ff6
ivletest <karakan@protonmail.com>
2020-05-15 09:10:40 +0200

    Working on new input manager

---
+++ Reload.Input/ReloadInputManager.cs
@@ -0,0 +1,55 @@
+﻿namespace Reload.Input
+{
+    using Reload.Game;
+    using Reload.Input.Configuration;
+    using Reload.Input.Source;
+    using Silk.NET.Input;
+    using Silk.NET.Input.Common;
+    using System;
+
+    public class ReloadInputManager
+    {
+        public event Action InputDevicesInitialized;
+
+        private IInputContext inputContext;
+
+        public GameBase Game { get; }
+        public Keyboard Keyboard { get; private set; }
+        public Mouse Mouse { get; private set; }
+        public ITextInputDevice TextInput { get; private set; }
+
+        public ReloadInputManager(IGame game)
+        {
+            Game = game as GameBase;
+            Game.Window.Load += InitializeDevices();
+        }
+
+        public Action InitializeDevices()
+        {
+            inputContext = Game.Window.CreateInput();
+
+            Keyboard = new Keyboard(inputContext.Keyboards[0]);
+            Mouse = new Mouse(inputContext.Mice[0], Game);
+
+            TextInput = Keyboard as ITextInputDevice;
+
+            return InputDevicesInitialized;
+        }
+
+        public void Initialize(
+            KeyboardConfiguration keyboardConfiguration,
+            MouseConfiguration mouseConfiguration)
+        {
+            Game.Activated += OnApplicationResumed;
+            Game.Deactivated += OnApplicationPaused;
+        }
+
+        private void OnApplicationPaused(object sender, EventArgs e)
+        {
+        }
+
+        private void OnApplicationResumed(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ Reload.Input/Source/ReloadKeyboard.cs
@@ -0,0 +1,67 @@
+﻿namespace Reload.Input.Source
+{
+    using Reload.Core.Collections;
+    using Silk.NET.Input.Common;
+    using System;
+    using System.Collections.Generic;
+
+    public class ReloadKeyboard
+    {
+        public Dictionary<Key, Action> keyActionPairs = new Dictionary<Key, Action>();
+
+        private const int initialBufferSize = 8;
+        private IKeyboard keyboardBase;
+
+        public FastList<Key> PressedKeys;
+        public FastList<Key> ReleasedKeys;
+        public FastList<Key> DownKeys;
+        public Dictionary<Key, int> RepeadKeys;
+
+        public ReloadKeyboard(IKeyboard keyboard)
+        {
+            keyboardBase = keyboard;
+            keyboardBase.KeyDown += HandleKeyDown;
+            keyboardBase.KeyUp += HandleKeyUp;
+
+            PressedKeys = new FastList<Key>(initialBufferSize);
+            ReleasedKeys = new FastList<Key>(initialBufferSize);
+            DownKeys = new FastList<Key>(initialBufferSize);
+            RepeadKeys = new Dictionary<Key, int>();
+        }
+
+        private void HandleKeyDown(IKeyboard keyboard, Key key, int arg)
+        {
+            PressedKeys.Add(key);
+        }
+
+        private void HandleKeyUp(IKeyboard keyboard, Key key, int arg)
+        {
+            if (PressedKeys.Count == 1)
+            {
+                keyActionPairs.TryGetValue(key, out Action action);
+                action?.Invoke();
+            }
+
+            PressedKeys.Remove(key);
+        }
+
+        private void HandleTextInput(IKeyboard keyboard, char character)
+        {
+
+        }
+
+        public void EnabledTextInput()
+        {
+            keyboardBase.KeyDown -= HandleKeyDown;
+            keyboardBase.KeyUp -= HandleKeyUp;
+            keyboardBase.KeyChar += HandleTextInput;
+        }
+
+        public void DisableTextInput()
+        {
+            keyboardBase.KeyChar -= HandleTextInput;
+            keyboardBase.KeyDown += HandleKeyDown;
+            keyboardBase.KeyUp += HandleKeyUp;
+        }
+    }
+}
